92314a9732babafa0b7d327ac4a9bef14689cd49
유용준 <55341412+YongJunYou@users.noreply.github.com>
2024-04-03 21:06:41 +0900

    Delete src/xl430_joy/build directory

--- src/xl430_joy/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yyj/catkin_ws/src/xl430_joy
-# Build directory: /home/yyj/catkin_ws/src/xl430_joy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
