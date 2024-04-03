1593b47cf64047eb6319efe8d7faf0b4bcde324c
유용준 <55341412+YongJunYou@users.noreply.github.com>
2024-04-03 21:06:09 +0900

    Delete src/mg966r_joy/build directory

--- src/mg966r_joy/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lmg966r_joy".split(';') if "-lmg966r_joy" != "" else []
-PROJECT_NAME = "mg966r_joy"
-PROJECT_SPACE_DIR = "/home/yyj/catkin_ws/src/mg966r_joy/build/devel"
-PROJECT_VERSION = "0.0.0"
